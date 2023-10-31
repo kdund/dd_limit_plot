c46e5fbb226c9e464f06765d003bf81e6c1348e9
Knut Dundas Moraa <knutdundasmoraa@gmail.com>
2023-10-31 15:23:43 -0400

    change plotter to retrieve metadata from central file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="dd_limit_plot",
+    version="0.1",
+    packages=find_packages(),
+    author='Knut Dundas Morå, Ibles Ocina, Xun Chen, Yue Ma',
+    author_email="knut.dundas.moraa@columbia.edu",
+    description="A collection of direct detection results, including a plotter and metadata",
+
+)
